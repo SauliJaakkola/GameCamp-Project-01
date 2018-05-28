52ad909d90e9eba9874a82b736dd5b71bcd8bd8e
Muskotti <36733106+Muskotti@users.noreply.github.com>
2018-05-28 12:57:45 +0300

    Player and walls

---
+++ Asteroid_Game/Assets/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	float moveHorizontal = 0f;
+	public float turn;
+	public float speed;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		moveHorizontal -= Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = transform.up * moveVertical;
+		transform.eulerAngles = new Vector3 (0f, 0f, moveHorizontal * turn);
+		rb.AddForce (movement * speed);
+	}
+}
